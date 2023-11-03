1c6a2556d4bdf4561867eeefec2da54fe158ec3b
Conrad Njogu <Njogu Mbugua>
2023-11-03 08:00:45 +0300

    dynamically allocate memory

--- prompt.c
+++ prompt.c
@@ -1,13 +1,15 @@
 #include "shell.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void prompt_display()
 {
   size_t n = 10;/*arbitrary length*/
-  char *buf = NULL;
+  char *buf = malloc(sizeof(size_t)*n);
 
   printf("$ ");
   getline(&buf, &n, stdin);
+  tokenize(buf);
 }
 
 
@@ -17,8 +19,8 @@ void tokenize(char *commands)
 
   while(pCommands != NULL) 
   {
-      printf("%s\n", pCommands);/*printf is for debugging purposes*/
-      pCommands = strtok(NULL, " ");
+    printf("%s\n", pCommands);/*printf is for debugging purposes*/
+    pCommands = strtok(NULL, " ");
   }
 }
 
