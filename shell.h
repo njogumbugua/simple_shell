81233f0217a451881ab5615b5a9e2a5c4293d674
Conrad Njogu <Njogu Mbugua>
2023-11-08 09:27:59 +0300

    remove function

--- shell.h
+++ shell.h
@@ -6,9 +6,11 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
 void start_shell(void);
 char *read_commands();
 char **tokenize_commands(char *line);
-void prompt_display(void);
 int execute_commands(char **commands);
 #endif
