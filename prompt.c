8da984456dab16610e70aa080cf02e24cd220c03
Conrad Njogu <Njogu Mbugua>
2023-11-10 05:41:03 +0300

    replace printf with write

--- prompt.c
+++ prompt.c
@@ -7,10 +7,11 @@ void start_shell()
 {
   char *input;
   char **commands;
+  char *prompt = "$ ";
 
   while (1)
   {
-    printf("$ ");
+    write(STDOUT_FILENO, prompt, 2);
     input = read_commands();
     commands = tokenize_commands(input);
     execute_commands(commands);
@@ -24,45 +25,51 @@ char *read_commands()
 {
   char *input = NULL;
   size_t bufsize = 0;
+  int bytes;
+  bytes = getline(&input, &bufsize, stdin);
 
-  if (getline(&input, &bufsize, stdin) == -1){
-    if (feof(stdin)) {
+  if ( bytes == -1)
+  {
+    if (feof(stdin))
+    {
       exit(EXIT_SUCCESS);
-    } else  {
+    } else {
       perror("readline");
       exit(EXIT_FAILURE);
     }
   }
+  if (input[bytes - 1] == '\n')
+    input[bytes - 1] = '\0';
 
   return (input);
 }
 
 char **tokenize_commands(char *line)
 {
-  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  int bufsize = TOKEN_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token;
 
   if (!tokens) {
-    fprintf(stderr, "allocation error\n");
+    perror("allocation error");
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, TOKEN_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
 
     if (position >= bufsize) {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += TOKEN_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-        fprintf(stderr, " allocation error\n");
+        perror("allocation error");
         exit(EXIT_FAILURE);
       }
     }
 
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, TOKEN_DELIM);
   }
   tokens[position] = NULL;
   return (tokens);
@@ -76,11 +83,12 @@ int execute_commands(char **commands)
   if (pid == 0)
   {
       char *args[2];
-      char *envp[1];
+      char *envp[2];
 
     args[0] = *commands;
     args[1] = NULL;
-    envp[0] = NULL;
+    envp[0] = "PATH=/bin";
+    envp[1] = NULL;
 
     if (execve(args[0], args, envp) == -1)
     {
@@ -93,6 +101,5 @@ int execute_commands(char **commands)
 
   return (1);
 }
-
 
 
