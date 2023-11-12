bc747c9da2d6ac398bdc665987a6dfafe8694a5a
Conrad Njogu <Njogu Mbugua>
2023-11-12 07:35:05 +0300

    refactor code base

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "shell.h"
 
-int main(void)
+int main()
 {
-  start_shell();
+  char *input;
+  char **commands;
+  char *prompt = "$ ";
+
+while (1)
+  {
+    if (isatty(STDIN_FILENO))
+    {
+      write(STDOUT_FILENO, prompt, 2);
+    }
+    input = read_commands();
+    commands = tokenize_commands(input);
+    execute_commands(commands);
+
+    free(input);
+    free(commands);
+  }
+
   return(0);
 }
