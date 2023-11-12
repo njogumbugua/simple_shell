cc6156745695083c8d687541d98ff1de7dc7b64c
Conrad Njogu <Njogu Mbugua>
2023-11-12 07:35:22 +0300

    refactor header file

--- shell.h
+++ shell.h
@@ -5,15 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define TOKEN_BUFSIZE 64
-#define TOKEN_DELIM " \t\r\n\a"
+#define TOKEN_DELIM " \t \r \n \a"
 
-void start_shell(void);
+extern char **environ;
+
 char *read_commands();
 char **tokenize_commands(char *line);
-int execute_commands(char **commands);
+int execute_commands(char **command);
 int change_dir(char **args);
+int file_exists(const char *filename);
 int exit_shell();
+char **search_path(char *path);
+
 #endif
