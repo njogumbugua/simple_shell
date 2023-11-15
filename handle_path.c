05f5d2861b4c9a05cd47d93a70dbf3cceec387b7
Conrad Njogu <Njogu Mbugua>
2023-11-15 15:46:33 +0300

    handle path function

--- handle_path.c
+++ handle_path.c
@@ -1,25 +1,24 @@
 #include "shell.h"
 
-char **search_path(char *path)
+void handle_path(char **commands, char *path)
 {
-  int bufsize = TOKEN_BUFSIZE, position = 0;
-  char *token;
-  char **tokens = malloc(bufsize * sizeof(char*));
-
-  path = getenv("PATH");
-
-  if (path == NULL) {
-      perror("Error");
-  }
-
-
-  token = strtok(path, ":");
+  char *token = strtok(path, ":");
 
   while (token != NULL) {
-    tokens[position] = token;
-    position++;
+    char *full_path =
+        malloc(str_len(token) + str_len("/") + str_len(commands[0]) + 1);
+    str_cpy(full_path, token);
+    str_cat(full_path, "/");
+    str_cat(full_path, commands[0]);
+
+    if (file_exists(full_path)) {
+      execve(full_path, commands, NULL);
+    }
+
+    free(full_path);
     token = strtok(NULL, ":");
   }
 
-  return(tokens);
+  print_f("command not found: %s\n", commands[0]);
+  exit(EXIT_FAILURE);
 }
