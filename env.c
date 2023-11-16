10078a37b53e4416946f500e375f4c3f9d55cf3f
Conrad Njogu <Njogu Mbugua>
2023-11-16 16:22:45 +0300

    betty compliant

--- env.c
+++ env.c
@@ -1,18 +1,21 @@
 #include "shell.h"
 
-void print_environment()
+/**
+ * print_environment - print environmen variables
+ */
+void print_environment(void)
 {
-  char **env_var = environ;
+char **env_var = environ;
 
-  while (*env_var != NULL)
-  {
-    size_t len = 0;
-    while ((*env_var)[len] != '\0')
-    {
-      len++;
-    }
-    write(STDOUT_FILENO, *env_var, len);
-    write(STDOUT_FILENO, "\n", 1);
-    env_var++;
-  }
+while (*env_var != NULL)
+{
+size_t len = 0;
+while ((*env_var)[len] != '\0')
+{
+len++;
+}
+write(STDOUT_FILENO, *env_var, len);
+write(STDOUT_FILENO, "\n", 1);
+env_var++;
+}
 }
