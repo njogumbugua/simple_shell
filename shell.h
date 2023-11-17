f2deaca056cb2bd6b5979e415eeb5ae13f858b4a
Conrad Njogu <Njogu Mbugua>
2023-11-17 21:53:02 +0300

    modify handle_path protopype

--- shell.h
+++ shell.h
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdarg.h>
 
@@ -19,14 +20,14 @@ char *read_commands();
 char **tokenize_commands(char *line);
 int execute_commands(char **command);
 int change_dir(char **args);
-bool file_exists(const char *filename);
+int file_exists(const char *filename);
 int str_cmp(const char *str1, const char *str2);
 char **search_path(char *path);
 void print_environment(void);
 size_t str_len(const char *str);
 char *str_cat(char *destination, const char *source);
 char *str_cpy(char *dest, const char *src);
-void handle_path(char **commands, char *path);
 void print_f(const char *format, ...);
+char *handle_path(const char *command);
 
 #endif
